07f64b09fa59136ae063c2890f15e1d02fa25ec1
LAPTOP-MKIRSEB3\gonis <g-nishimura@seassist.co.jp>
2025-03-03 21:47:29 +0900

    fix 2.Spring スターター・プロジェクト課題 / 02FizzBuzz

--- src/main/java/com/example/demo/fizzBuss/service/FizzBuzzService.java
+++ src/main/java/com/example/demo/fizzBuss/service/FizzBuzzService.java
@@ -8,17 +8,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class FizzBuzzService {
 
-	public List<String> fizzBuzz(int num) {
+	public List<String> fizzBuzz(int max) {
 		List<String> numbersList = new ArrayList<>();
-		for (int i = 0; i <= num; i++) {
-			if (i % 15 == 0) {
+		for (int num = 0; num <= max; num++) {
+			if (num % 15 == 0) {
 				numbersList.add("FizzBuzz");
-			} else if (i % 5 == 0) {
+			} else if (num % 5 == 0) {
 				numbersList.add("Buzz");
-			} else if (i % 3 == 0) {
+			} else if (num % 3 == 0) {
 				numbersList.add("Fizz");
 			} else {
-				numbersList.add(String.valueOf(i));
+				numbersList.add(String.valueOf(num));
 			}
 		}
 		return numbersList;
